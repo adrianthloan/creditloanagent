3f196f414e478694724e35aa09218a28e8bdd833
Yong-suk.Jung <yong-suk.jung@linefriends.com>
2023-02-27 12:01:49 +0900

    fix img overflow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
   import Search from "$lib/components/Search.svelte";
 </script>
 
-<div class="visual_wrap overflow-hidden">
-    <div class="visual_bg">
+<div class="visual_wrap w-full">
+    <div class="visual_bg w-full overflow-hidden">
         <img alt="main" class="max-w-none" src="/main_visual.jpg"/>
     </div>
 </div>
